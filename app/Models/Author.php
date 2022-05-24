f4a5e3aa21c46f9fe021a4467f622c664a480023
joro <jorkomzx@gmail.com>
2022-05-24 14:51:03 +0300

    wip Author

---
+++ app/Models/Author.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function setDobAttribute($dob)
+    {
+        $this->attributes['dob'] = Carbon::parse($dob);
+    }
+
+    protected $dates = [
+      'dob'
+    ];
+}
---
+++ tests/Feature/AuthorMangementTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorMangementTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/author',[
+           'name' => 'Author Name',
+           'dob' => '05/14/1988'
+        ]);
+
+        $authors = Author::all();
+        $this->assertCount(1,$authors);
+        $this->assertInstanceOf(Carbon::class,$authors->first()->dob);
+        $this->assertEquals('1988/05/14',Carbon::parse($authors->first()->dob)->format('Y/m/d'));
+    }
+}
